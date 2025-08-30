567c2b11b1dae28497d55ec90fad917d8805369e
ehtishamalik <malikehtishamali334@hotmail.com>
2025-08-30 13:46:56 +0500

    Implement authentication flow with Google sign-in and set up database schema

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,17 @@
+"use client";
+
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import { authClient } from "@/lib/auth-client";
 
 const Auth = () => {
+  const handleSignIn = async () => {
+    return await authClient.signIn.social({
+      provider: "google",
+    });
+  };
+
   return (
     <main className="sign-in">
       <aside className="testimonial">
@@ -64,7 +73,7 @@ const Auth = () => {
             Create and share your very first <span>SnapCast video</span> in no
             time!
           </p>
-          <button>
+          <button onClick={handleSignIn}>
             <Image
               src="/assets/icons/google.svg"
               alt="Google"
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,13 @@
 import { db } from "@/drizzle";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import * as schema from "@/drizzle/schema";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
+    schema,
   }),
   socialProviders: {
     google: {
@@ -12,4 +15,6 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
+  plugins: [nextCookies()],
+  baseURL: process.env.NEXT_PUBLIC_VERCEL_URL as string,
 });
